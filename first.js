f8678f6cebe2b983f0ff5c0493eba6e776b37f05
DustinTLee <Lee.Dustin@gmail.com>
2021-01-03 16:03:49 -0500

    array testing

--- first.js
+++ first.js
@@ -1,3 +1,9 @@
 var catBreed;
-catBreed = "mank";
+catBreed = "maine coon";
 console.log(catBreed);
+var list = [1, 2, 3];
+console.log(list[0]);
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var i = list_1[_i];
+    console.log(i);
+}
--- first.ts
+++ first.ts
@@ -1,5 +1,18 @@
 var catBreed: String;
 
-catBreed = "mank";
+catBreed = "maine coon";
 
 console.log(catBreed)
+
+let list: number[] = [1, 2, 3];
+
+console.log(list[0])
+
+for (let i of list){
+    console.log(i);
+}
+
+let stringList: string[] =["asd","asd2"];
+for (let i of stringList){
+    console.log(i);
+}
