969d610d43e3ac139a3a2b3fb8ce477e58189daa
DustinTLee <Lee.Dustin@gmail.com>
2021-01-17 15:48:49 -0500

    class enhancements

--- weapon.ts
+++ weapon.ts
@@ -6,4 +6,12 @@ export class Weapon {
         this.averageDamage = averageDamage
         this.weaponName = weaponName
     }
+
+    getWeaponAttackString(strengthModifier: number){
+        var totalAverageDamage: number = this.averageDamage + strengthModifier
+
+        var returnString: string = " He deals " + totalAverageDamage + " with his " + this.weaponName
+        
+        return returnString
+    }
 }
